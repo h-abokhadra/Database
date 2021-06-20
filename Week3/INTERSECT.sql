2f695d969bdc8d6040556eade716665e7fb41e47
h-abokhadra <59623466+h-abokhadra@users.noreply.github.com>
2021-06-19 21:49:36 -0400

    Create INTERSECT.sql

---
+++ Week3/INTERSECT.sql
@@ -0,0 +1,8 @@
+/* using INTERSECT*/
+   SELECT cust_id, cust_first_name, cust_last_name
+     FROM sh.customers
+    WHERE cust_id in (101, 102, 103)
+INTERSECT
+   SELECT cust_id, cust_first_name, cust_last_name
+     FROM sh.customers
+    WHERE cust_id in (103, 104, 105);
