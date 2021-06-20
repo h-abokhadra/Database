3e05f85fe2839ac487bbc848f88cf15b425b3653
h-abokhadra <59623466+h-abokhadra@users.noreply.github.com>
2021-06-19 21:49:49 -0400

    Create MINUS.sql

---
+++ Week3/MINUS.sql
@@ -0,0 +1,9 @@
+
+/* using MINUS */
+SELECT cust_id, cust_first_name, cust_last_name
+  FROM sh.customers
+ WHERE cust_id in (101, 102, 103)
+ MINUS
+SELECT cust_id, cust_first_name, cust_last_name
+  FROM sh.customers
+ WHERE cust_id in (103, 104, 105);
