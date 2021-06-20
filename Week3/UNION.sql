2ca9cb633b95a2123fc159b56003dc6cef726e1f
h-abokhadra <59623466+h-abokhadra@users.noreply.github.com>
2021-06-19 21:50:09 -0400

    Create UNION.sql

---
+++ Week3/UNION.sql
@@ -0,0 +1,9 @@
+
+/* using UNION */
+SELECT cust_id, cust_first_name, cust_last_name
+  FROM sh.customers
+ WHERE cust_id in (101, 102, 103)
+ UNION
+SELECT cust_id, cust_first_name, cust_last_name
+  FROM sh.customers
+ WHERE cust_id in (103, 104, 105);
