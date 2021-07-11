e2a1dc9687a859bec00ac43c62a26eebbdce83fb
h-abokhadra <59623466+h-abokhadra@users.noreply.github.com>
2021-07-10 22:28:16 -0400

    Create Q2.sql

---
+++ Assignment1/Q2.sql
@@ -0,0 +1,11 @@
+SELECT custname,
+CASE COUNT(carserial)
+                            WHEN 1 
+                                      THEN 'one-time buyer'
+                            WHEN 2 
+                                     THEN 'two-time buyer'
+                            ELSE 'frequent buyer'
+                END 
+FROM SI.CAR
+WHERE carmake='MERCEDES'
+GROUP BY custname ;
