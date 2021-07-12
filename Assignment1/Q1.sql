385c3e952ab23b1295c7daa6981fd97184043631
h-abokhadra <59623466+h-abokhadra@users.noreply.github.com>
2021-07-11 21:33:33 -0400

    Update Q1.sql

--- Assignment1/Q1.sql
+++ Assignment1/Q1.sql
@@ -1,10 +1,8 @@
-SELECT SUM(PARTSCOST), SUM(LABORCOST), AVG(PARTSCOST), AVG(LABORCOST), COUNT(SERVINVNO)
-FROM SI.servinv
-WHERE (carserial=' Acura' OR carserial =' Mercedes' OR carserial ='Jaguar ') AND
-servdate BETWEEN '2015-09-01' AND '2018-12-31' ;
-
-SELECT sum(carsaleprice), avg(partscost + laborcost), count(custname)
-FROM servinv S, car C, saleinr Sale
-WHERE S.carserial = C. carserial = Sale.carserial AND S.custname = C. custname = Sale.custname
-AND carmake IN('Acura', 'Mercedes', 'Jaguar')
-AND servdate BETWEEN '2015-09-01' AND '2018-12-31' ;
+SELECT SUM(PARTSCOST+LABORCOST) "SUM", ROUND (AVG(PARTSCOST+LABORCOST),2) "AVG", COUNT(SERVINVNO) "COUNT"
+FROM SI.servinv s
+JOIN SI.car c
+ON s.carserial = c.carserial
+JOIN SI.purchinv p
+ON c.purchinvno = p.purchinvno
+WHERE c.carmake IN ('ACURA', 'MERCEDES', 'JAGUAR ') AND
+s.servdate BETWEEN '2015-09-01' AND '2018-12-31' ;
